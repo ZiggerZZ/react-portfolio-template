6c1116f3d477929a42a123a299fea56e77bd76f6
Zigfrid Zvezdin <ziggerZZ@gmail.com>
2022-02-02 22:24:35 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Zvezdin`,
+    description: `A personal website of Zigfrid Zvezdin.`,
+    author: `@ziggerzz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
