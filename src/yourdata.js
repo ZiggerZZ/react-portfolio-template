4a6738bdfde02b6b609467c7ff54a2eff849c3cf
Zigfrid Zvezdin <ziggerZZ@gmail.com>
2022-02-02 22:04:33 +0100

    Update yourdata.js

--- src/yourdata.js
+++ src/yourdata.js
@@ -17,21 +17,21 @@ export default {
   //Change The Website Template
 
   //   Header Details ---------------------
-  name: "chetan",
+  name: "Zigfrid",
   headerTagline: [
     //Line 1 For Header
-    "Building digital",
+    "Building AI,",
     //Line 2 For Header
-    "products, brands,",
+    "doing parkour",
     //Line 3 For Header
-    "and experience",
+    "and travelling",
   ],
   //   Header Paragraph
   headerParagraph:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    "I'm currently looking for interesting projects to take part in 🕵🏻‍♂️",
 
   //Contact Email
-  contactEmail: "hello@chetanverma.com",
+  contactEmail: "zigfrid.zvezdin+site@kes.polytechnique.fr",
 
   // End Header Details -----------------------
 
@@ -45,7 +45,7 @@ export default {
       imageSrc:
         "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://react-portfolio-template-one.vercel.app/",
     },
     {
       title: "Project Two", //Project Title - Add Your Project Title Here
@@ -55,7 +55,7 @@ export default {
       imageSrc:
         "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://react-portfolio-template-one.vercel.app/",
     },
     {
       title: "Project Three", //Project Title - Add Your Project Title Here
@@ -65,7 +65,7 @@ export default {
       imageSrc:
         "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://react-portfolio-template-one.vercel.app/",
     },
     {
       title: "Project Four", //Project Title - Add Your Project Title Here
@@ -75,7 +75,7 @@ export default {
       imageSrc:
         "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://react-portfolio-template-one.vercel.app/",
     },
     {
       title: "Project Five", //Project Title - Add Your Project Title Here
@@ -85,7 +85,7 @@ export default {
       imageSrc:
         "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://react-portfolio-template-one.vercel.app/",
     },
     {
       title: "Project Six", //Project Title - Add Your Project Title Here
@@ -95,7 +95,7 @@ export default {
       imageSrc:
         "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://react-portfolio-template-one.vercel.app/",
     },
 
     /*
@@ -113,11 +113,11 @@ export default {
 
   // About Secton --------------
   aboutParaOne:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "I did Moscow Institute of Physics and Technology in Russia and Ecole polytechnique in France.",
   aboutParaTwo:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "I'm a lifelong learner.",
   aboutParaThree:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    "What would you like to know more about me?",
   aboutImage:
     "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
 
@@ -164,9 +164,9 @@ export default {
 
   //   Promotion Section --------------------------
 
-  promotionHeading: "Heading",
+  promotionHeading: "Parkour",
   promotionPara:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "I started parkour in 2019, there are some pictures and videos on my Instagram.",
   // End Promotion Section -----------------
 
   //   Contact Section --------------
@@ -174,18 +174,18 @@ export default {
   contactSubHeading: "Let's create your next experience together",
   social: [
     // Add Or Remove The Link Accordingly
-    { img: githubIcon, url: "https://github.com/chetanverma16" },
-    {
-      img: codepenIcon,
-      url: "https://www.codepen.com/",
-    },
-    {
-      img: dribbbleIcon,
-      url: "https://dribbble.com/chetanverma",
-    },
+    { img: githubIcon, url: "https://github.com/ZiggerZZ" },
+//     {
+//       img: codepenIcon,
+//       url: "https://www.codepen.com/",
+//     },
+//     {
+//       img: dribbbleIcon,
+//       url: "https://dribbble.com/chetanverma",
+//     },
     {
       img: instagramIcon,
-      url: "https://www.instagram.com/",
+      url: "https://www.instagram.com/ZiggerZZ",
     },
   ],
 
